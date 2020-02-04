38698b33179a4f592365e9c5058b0c38733dd730
mbyk <marubayashi@gmail.com>
2020-02-04 21:33:15 +0900

    add app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+require "sinatra"
+require "sinatra/reloader"
+
+
+# index
+get "/" do 
+
+end
+
+# show 
+get "/memos/:id" do |memo_id|
+
+end
+
+# new
+get "/memos/new" do
+
+end
+
+# edit
+get "/memos/:id/edit" do |memo_id|
+
+end
+
+# destroy
+delete "memos/:id" do |memo_id|
+
+end
+
+# update
+patch "memos/:id" do |memo_id|
+
+end
+
+# create
+post "memos" do
+
+end
+
+
+
