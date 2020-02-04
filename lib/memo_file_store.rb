ef820ba9fade93fb71815738cd285a01c25d3eea
mbyk <marubayashi@gmail.com>
2020-02-04 22:31:34 +0900

    add stores'class

---
+++ lib/memo_file_store.rb
@@ -0,0 +1,25 @@
+require_relative "../models/memo.rb"
+
+class MemoFileStore
+
+  def findAll
+
+  end
+
+  def find(name_id)
+
+  end
+
+  def delete(memo_id)
+
+  end
+
+  def update(memo)
+
+  end
+
+  def create(memo)
+
+  end
+    
+end
