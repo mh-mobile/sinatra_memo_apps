98ef1c61743b4a07b7c7a2934496c8b56b6ff282
mbyk <marubayashi@gmail.com>
2020-02-04 22:16:06 +0900

    add memos_controller.rb

---
+++ controller/memos_controller.rb
@@ -0,0 +1,36 @@
+class MemosController
+
+
+  def index
+    "index"
+  end
+
+  def new
+    "new"
+  end
+
+  def edit
+    "edit"
+  end
+
+
+  def destroy
+    "destroy"
+  end
+
+
+  def update 
+    "update"
+  end
+
+  def create
+    "create"
+  end
+
+
+  def show
+    "show"
+  end
+
+
+end
