079da6a9c366778771f67e4192cbc8c8e35f5140
mbyk <marubayashi@gmail.com>
2020-02-04 22:14:39 +0900

    rename app.js app.rb

--- app.rb
+++ app.rb
@@ -1,41 +1,46 @@
 require "sinatra"
 require "sinatra/reloader"
+require_relative "controller/memos_controller.rb"
 
+
+memos_controller = MemosController.new
 
 # index
 get "/" do 
+  redirect "/memos"
+end
 
+get "/memos" do 
+  memos_controller.index
 end
 
 # show 
 get "/memos/:id" do |memo_id|
-
+  memos_controller.show
 end
 
 # new
 get "/memos/new" do
-
+  memos_controller.new
 end
 
 # edit
 get "/memos/:id/edit" do |memo_id|
-
+  memos_controller.edit
 end
 
 # destroy
 delete "memos/:id" do |memo_id|
-
+  memos_controller.destroy
 end
 
 # update
 patch "memos/:id" do |memo_id|
-
+  memos_controller.update
 end
 
 # create
 post "memos" do
-
+  memos_controller.create
 end
-
-
 
