4cf5ab057bc8926a87400456ededc747b0c80cfb
mbyk <marubayashi@gmail.com>
2020-02-07 14:21:08 +0900

    rename memo_recordable memo_storable

--- models/memo.rb
+++ models/memo.rb
@@ -1,8 +1,8 @@
-require_relative "../lib/memo_recordable.rb"
+require_relative "../lib/memo_storable.rb"
 require_relative "../lib/memo_file_store.rb"
 
 class Memo
-  extend MemoRecordable
+  extend MemoStorable
   attr_accessor :memo_id, :content, :created_at, :updated_at
   store_strategy MemoFileStore.new
 end
