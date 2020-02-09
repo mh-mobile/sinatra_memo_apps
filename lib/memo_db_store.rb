46d871ea919d95841fe04f08947d7e026c19a350
mbyk <marubayashi@gmail.com>
2020-02-10 00:23:02 +0900

    apply rubocop

--- lib/memo_db_store.rb
+++ lib/memo_db_store.rb
@@ -6,23 +6,22 @@ require "erb"
 require "dotenv"
 
 class MemoDbStore
-
   attr_accessor :connection
   attr_accessor :db_config
 
   def initialize(db_config_path)
     Dotenv.load
-    @db_config = YAML::load(ERB.new(File.read(db_config_path)).result)["db"]
+    @db_config = YAML.load(ERB.new(File.read(db_config_path)).result)["db"]
   end
 
   def findAll
     connection = PG.connect(db_config)
-    results = connection.exec("select * from memos") 
+    results = connection.exec("select * from memos")
     results.map(&method(:convert))
   rescue PG::Error => e
     puts e.message
-  ensure 
-    connection.close if connection 
+  ensure
+    connection.close if connection
   end
 
   def find(memo_id)
@@ -31,8 +30,8 @@ class MemoDbStore
     convert(results.first) if results.first
   rescue PG::Error => e
     puts e.message
-  ensure 
-    connection.close if connection 
+  ensure
+    connection.close if connection
   end
 
   def delete(memo_id)
@@ -40,8 +39,8 @@ class MemoDbStore
     connection.exec_params("delete from memos where id = $1", [memo_id])
   rescue PG::Error => e
     puts e.message
-  ensure 
-    connection.close if connection 
+  ensure
+    connection.close if connection
   end
 
   def update(memo_id, content)
@@ -49,8 +48,8 @@ class MemoDbStore
     connection.exec_params("update memos set content = $1, updated_at = $2 where id = $3", [content,  Time.now.iso8601, memo_id])
   rescue PG::Error => e
     puts e.message
-  ensure 
-    connection.close if connection 
+  ensure
+    connection.close if connection
   end
 
   def create(content)
@@ -61,19 +60,17 @@ class MemoDbStore
     convert(results.first) if results.first
   rescue PG::Error => e
     puts e.message
-  ensure 
-    connection.close if connection 
+  ensure
+    connection.close if connection
   end
 
-  private 
-
-  def convert(result_row)
-    {
-      "memo_id" => result_row["id"],
-      "content" => result_row["content"],
-      "created_at" => result_row["created_at"],
-      "updated_at" => result_row["updated_at"]
-    }
-  end
-
+  private
+    def convert(result_row)
+      {
+        "memo_id" => result_row["id"],
+        "content" => result_row["content"],
+        "created_at" => result_row["created_at"],
+        "updated_at" => result_row["updated_at"]
+      }
+    end
 end
