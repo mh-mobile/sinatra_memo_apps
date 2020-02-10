6f090679ec0e31abbcc72a304fdba0590d5d43ab
mbyk <marubayashi@gmail.com>
2020-02-10 22:14:41 +0900

    delete unnecessory comment

--- models/memo_dao.rb
+++ models/memo_dao.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "../lib/memo_storable.rb"
-# require_relative "../lib/memo_file_store.rb"
 require_relative "../lib/memo_db_store.rb"
 
 class MemoDao
   extend MemoStorable
-  # store_strategy MemoFileStore.new("resource/memo.json")
   store_strategy MemoDbStore.new(File.dirname(__FILE__) + "/../resource/database.yml")
 end
