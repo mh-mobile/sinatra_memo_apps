8894da641b69521ecd2278d4bd9cfc9668cff8c3
mbyk <marubayashi@gmail.com>
2020-02-15 18:03:56 +0900

    change create's udpate using RETURNING

--- lib/memo_db_store.rb
+++ lib/memo_db_store.rb
@@ -42,8 +42,7 @@ class MemoDbStore
 
   def create(content)
     PGManager.open(db_config) do |connection|
-      connection.exec_params("insert into memos (content, created_at, updated_at) values ($1, $2, $3)", [content, Time.now.iso8601, Time.now.iso8601])
-      results = connection.exec("select LASTVAL() from memos")
+      results = connection.exec_params("insert into memos (content, created_at, updated_at) values ($1, $2, $3) RETURNING id", [content, Time.now.iso8601, Time.now.iso8601])
       results = connection.exec_params("select * from memos where id = $1", [results.first["id"]])
       convert(results.first) if results.first
     end
